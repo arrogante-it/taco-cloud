bea854d937fb39dbea73fb1077ae27260ee2ffb3
Alexandr <alexandrsinko2000@gmail.com>
2023-05-03 22:17:22 +0300

    add new functionality 2

--- src/main/java/com/arroganteit/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/arroganteit/tacocloud/web/DesignTacoController.java
@@ -7,8 +7,10 @@ import com.arroganteit.tacocloud.taco.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -64,8 +66,14 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco,
+    public String processTaco(@Valid Taco taco,
+                              Errors errors,
                               @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
